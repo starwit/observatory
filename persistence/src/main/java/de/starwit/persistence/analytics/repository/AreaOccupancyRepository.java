248d89a8fe900740d370d5bc2d422f95eb2bafc2
witchpou <anett_huebner@gmx.de>
2023-12-03 16:20:36 +0100

    remove transactional

--- persistence/src/main/java/de/starwit/persistence/analytics/repository/AreaOccupancyRepository.java
+++ persistence/src/main/java/de/starwit/persistence/analytics/repository/AreaOccupancyRepository.java
@@ -2,8 +2,6 @@ package de.starwit.persistence.analytics.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import de.starwit.persistence.analytics.entity.AreaOccupancyEntity;
 
@@ -11,7 +9,6 @@ import de.starwit.persistence.analytics.entity.AreaOccupancyEntity;
  * AreaOccupancy Repository class
  */
 @Repository
-@Transactional(transactionManager = "analyticsTransactionManager", propagation = Propagation.REQUIRES_NEW)
 public interface AreaOccupancyRepository extends JpaRepository<AreaOccupancyEntity, Long> {
 
 }
