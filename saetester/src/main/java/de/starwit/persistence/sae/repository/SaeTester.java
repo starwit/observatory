1dfa32df25e66e22a7fb14064f8e652e7a945dab
witchpou <anett_huebner@gmx.de>
2023-12-05 18:57:17 +0100

    added tester

---
+++ saetester/src/main/java/de/starwit/persistence/sae/repository/SaeTester.java
@@ -0,0 +1,44 @@
+package de.starwit.persistence.sae.repository;
+
+import java.sql.Timestamp;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Repository
+public class SaeTester {
+
+    @PersistenceContext(unitName = "sae")
+    EntityManager em;
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private String INSERT_QUERY = "INSERT INTO detection(capture_ts, class_id, confidence, object_id, min_x, min_y, max_x, max_y, camera_id) VALUES (?,?,?,?,?,?,?,?,?)";
+
+    @Scheduled(initialDelay = 100, fixedRate = 1000)
+    @Transactional
+    public void insertDetectionData() {
+        Timestamp captureTimestamp = new Timestamp(System.currentTimeMillis());
+
+        Query q = em.createNativeQuery(INSERT_QUERY);
+        q.setParameter(1, captureTimestamp);
+        q.setParameter(2, 2);
+        q.setParameter(3, 0.0f);
+        q.setParameter(4, "unknown");
+        q.setParameter(5, 0);
+        q.setParameter(6, 1);
+        q.setParameter(7, 0);
+        q.setParameter(8, 1);
+        q.setParameter(9, "RangelineSMedicalDr");
+        q.executeUpdate();
+        log.info("Added Entry to database");
+    }
+
+}
