0f8b97193abfc68b59d081dc10e2c483a1c74468
flonix8 <flstanek@googlemail.com>
2024-05-08 14:24:16 +0200

    Add response code logging

--- service/src/main/java/de/starwit/service/geojson/GeoJsonSenderService.java
+++ service/src/main/java/de/starwit/service/geojson/GeoJsonSenderService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -53,11 +54,13 @@ public class GeoJsonSenderService {
 
         try {
             String geoJsonString = objectMapper.writeValueAsString(geoJson);
-            restClient.post()
+            ResponseEntity<Void> response = restClient.post()
                 .uri(GEO_JSON_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(geoJsonString)
-                .retrieve();
+                .retrieve().toBodilessEntity();
+            
+            log.info("GeoJson response code: {}", response.getStatusCode());
         } catch (JsonProcessingException e) {
             log.error("Failed to send GeoJson", e);
         }
