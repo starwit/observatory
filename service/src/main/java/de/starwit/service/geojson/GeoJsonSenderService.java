6012be7b0c75fc366b214e06db135d82bd1c7e70
flonix8 <flstanek@googlemail.com>
2024-05-09 10:26:17 +0200

    Make HttpClient ignore all TLS errors...

--- service/src/main/java/de/starwit/service/geojson/GeoJsonSenderService.java
+++ service/src/main/java/de/starwit/service/geojson/GeoJsonSenderService.java
@@ -1,14 +1,33 @@
 package de.starwit.service.geojson;
 
 import java.net.URI;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.geojson.GeoJsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -42,7 +61,7 @@ public class GeoJsonSenderService {
         requestFactory.setReadTimeout(Duration.ofMillis(2000));
         
         this.restClient = RestClient.builder()
-            .requestFactory(requestFactory)
+            .requestFactory(makeTlsErrorIgnoringRequestFactory())
             .build();
     }
     
@@ -64,5 +83,40 @@ public class GeoJsonSenderService {
         } catch (JsonProcessingException e) {
             log.error("Failed to send GeoJson", e);
         }
+    }
+
+    private ClientHttpRequestFactory makeTlsErrorIgnoringRequestFactory() {
+        TrustStrategy acceptingTrustStrategy = (chain, authType) -> true;
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContextBuilder.create().loadTrustMaterial(acceptingTrustStrategy).build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            log.error("Failed to create SSL context", e);
+            throw new RuntimeException("Failed to create SSL context", e);
+        }
+
+        SSLConnectionSocketFactory socketFactory = SSLConnectionSocketFactoryBuilder.create()
+            .setSslContext(sslContext)
+            .setHostnameVerifier(new NoopHostnameVerifier())
+            .build();
+
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+            .register("https", socketFactory)
+            .register("http", new PlainConnectionSocketFactory())
+            .build();
+
+        BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(socketFactoryRegistry);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setConnectionManager(connectionManager)
+            .build();
+        
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(Duration.ofMillis(2000));
+        requestFactory.setConnectionRequestTimeout(Duration.ofMillis(2000));
+        requestFactory.setHttpClient(httpClient);
+        
+        return requestFactory;
     }
 }
