e4e68f54d9c4f447c15512ec5f26ca5b6915a200
flonix8 <flstanek@googlemail.com>
2024-12-06 08:37:22 +0100

    Fix missing import

--- service/src/main/java/de/starwit/service/jobs/LineCrossingRunner.java
+++ service/src/main/java/de/starwit/service/jobs/LineCrossingRunner.java
@@ -7,6 +7,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
