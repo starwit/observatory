5f927afffea669fb82d0d6bb680ed75bc2c10f34
Anett Hübner <anett_huebner@gmx.de>
2025-12-04 11:47:12 +0100

    used info logging instead of info

--- service/src/main/java/de/starwit/service/analytics/AreaOccupancyService.java
+++ service/src/main/java/de/starwit/service/analytics/AreaOccupancyService.java
@@ -29,7 +29,7 @@ public class AreaOccupancyService {
 
     @Transactional("analyticsTransactionManager")
     public void addEntry(ObservationJobEntity jobEntity, ZonedDateTime occupancyTime, Long count) {
-        log.debug("Detected {} objects of class {} in area (area={}, name={})", count, jobEntity.getDetectionClassId(), jobEntity.getObservationAreaId(), jobEntity.getName());
+        log.info("Detected {} objects of class {} in area (area={}, name={})", count, jobEntity.getDetectionClassId(), jobEntity.getObservationAreaId(), jobEntity.getName());
         
         MetadataEntity metadata = metadataService.saveMetadataForJob(jobEntity);
         
@@ -58,7 +58,7 @@ public class AreaOccupancyService {
 
     @Transactional("analyticsTransactionManager")
     public void updateCountFromFlow(ObservationJobEntity jobEntity, ZonedDateTime occupancyTime, Direction direction) {
-        log.debug("Detected flow in direction {} of class {} in area (area={}, name={})", direction, jobEntity.getDetectionClassId(), jobEntity.getObservationAreaId(), jobEntity.getName());
+        log.info("Detected flow in direction {} of class {} in area (area={}, name={})", direction, jobEntity.getDetectionClassId(), jobEntity.getObservationAreaId(), jobEntity.getName());
         
         MetadataEntity metadata = metadataService.findFirstByNameAndClassification(jobEntity.getName(), jobEntity.getClassification());
         if (metadata == null) {
